e2f5791bb8564897b49a25a2c79b71ed3b6ddbc1
Taranjeet Singh Bedi <taranjeetsinghbedi2@gmail.com>
2025-04-25 17:17:16 +0530

    Updated:Doctor interface to include additional fields

--- src/types/doctor.ts
+++ src/types/doctor.ts
@@ -1,13 +1,24 @@
 export interface Doctor {
-  id: string;
   name: string;
+  name_initials: string;
   photo: string;
   specialities: { name: string }[];
-  video_consult: boolean;
-  in_clinic: boolean;
   experience: string;
+  doctor_introduction?: string;
+  languages?: string[];
+  video_consult?: boolean;
+  in_clinic?: boolean;
   fees: string;
+  clinic: {
+    name: string;
+    address: {
+      address_line1: string;
+      locality: string;
+      city: string;
+      logo_url?: string;
+      location?: string;
+    };
+  };
 }
-
 export type ConsultationMode = "video" | "in_clinic";
 export type SortOption = "fees" | "experience";
